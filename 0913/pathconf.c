f0e69d9c252c97e788a4a7dff9170d02b71410ff
ft1284595 <ft1284595@gmail.com>
2018-11-07 17:24:37 -0600

    1107181722

--- 0913/pathconf.c
+++ 0913/pathconf.c
@@ -4,8 +4,11 @@
 
 int main(void)
 {
+	int fd[2];
+	pipe(fd);
+	printf("pipe buf =%ld\n", fpathconf(fd[0], _PC_PIPE_BUF));
 	//printf("%ld\n", fpathconf(STDOUT_FILENO, _PC_NAME_MAX));
-	printf("%ld\n", pathconf(STDOUT_FILENO, _PC_NAME_MAX));
+	printf("%ld\n", pathconf("hello", _PC_NAME_MAX));
 
 
 	return 0;
--- 0915/group.c
+++ 0915/group.c
@@ -13,19 +13,29 @@ int main(void)
 			break;
 	}
 	if(pid == 0){
-		while(1){
+		{
 			printf("I am child %d\n", getpid());
 			sleep(3);
 		}
 	}
 	else if(pid > 0){
+		printf("n = %d\n", n);
+		n ++;
 		pid_t pid_c;
 		while(1){
-			pid_c = wait(NULL);
+			//pid_c = wait(NULL);
+			printf("I am parent \n");
+			pid_c = waitpid(0, NULL, WNOHANG);
+			if(pid_c > 0){
+				n++;
+			}
 			if(pid_c == -1)
-				break;
+				continue;
 			else
 				printf("wait for child %d\n", pid_c);
+			if(n == 3)
+				break;
+			sleep(1);
 		}
 	}
 
